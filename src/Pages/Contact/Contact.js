56ee462d8494036eb9e47aeb16f0985626d4d07d
suhanasalma <suhanasalma800@gmail.com>
2022-12-11 00:57:36 +0600

    contact form created

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-   const { register, handleSubmit } = useForm();
-   const handleEmail = (data) =>{
-      console.log(data)
+    const form = useRef();
+
+
+   const handleEmail = (e) =>{
+      e.preventDefault()
+      console.log('clicked')
+
+      emailjs
+        .sendForm(
+          "service_1um5kvc",
+          "template_jpm3g1k",
+          form.current,
+          "TAuocEhcbueQ-rF5d"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
    }
+
+
+
    return (
      <div>
-       <section
-         class="bg-cover mb-10 w-5/6 mx-auto bg"
-       >
+       <section class="bg-cover mb-10 w-5/6 mx-auto bg">
          <div class="flex flex-col min-h-screen bg-black/60">
            <div class="container flex flex-col flex-1 px-6 py-12 mx-auto">
              <div class="flex-1 lg:flex lg:items-center lg:-mx-6">
@@ -25,6 +45,73 @@ const Contact = () => {
 
                  <div class="mt-6 md:mt-8">
                    <h3 class="text-gray-300 ">Contact Me</h3>
+                   <div class="mt-6 space-y-8 md:mt-8">
+                     <p class="flex items-start -mx-2 text-white">
+                       <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         class="text-white w-6 h-6 mx-2"
+                         fill="none"
+                         viewBox="0 0 24 24"
+                         stroke="currentColor"
+                         stroke-width="2"
+                       >
+                         <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                         />
+                         <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                         />
+                       </svg>
+
+                       <span class="mx-2truncate w-72 dark:text-gray-400 text-white">
+                         Dhaka, Bangladesh
+                       </span>
+                     </p>
+
+                     <p class="flex items-start -mx-2">
+                       <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         class="w-6 h-6 mx-2 text-white"
+                         fill="none"
+                         viewBox="0 0 24 24"
+                         stroke="currentColor"
+                         stroke-width="2"
+                       >
+                         <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                         />
+                       </svg>
+
+                       <span class="mx-2 text-white">(+880) 1518390570 </span>
+                     </p>
+
+                     <p class="flex items-start -mx-2">
+                       <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         class="w-6 h-6 mx-2 text-white"
+                         fill="none"
+                         viewBox="0 0 24 24"
+                         stroke="currentColor"
+                         stroke-width="2"
+                       >
+                         <path
+                           stroke-linecap="round"
+                           stroke-linejoin="round"
+                           d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                         />
+                       </svg>
+
+                       <span class="mx-2 text-white">
+                         suhanasalma800@gmail.com
+                       </span>
+                     </p>
+                   </div>
 
                    <div class="flex mt-4 -mx-1.5 ">
                      <a
@@ -95,13 +182,14 @@ const Contact = () => {
                      Contact form
                    </h1>
 
-                   <form class="mt-6 ">
+                   <form ref={form} class="mt-6 " onSubmit={handleEmail}>
                      <div class="flex-1">
                        <label class="block mb-2 text-sm text-gray-600 ">
                          Full Name
                        </label>
                        <input
-                         {...register("name")}
+                         required
+                         name="name"
                          type="text"
                          placeholder="John Doe"
                          class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
@@ -113,8 +201,9 @@ const Contact = () => {
                          Email address
                        </label>
                        <input
-                         {...register("email")}
                          type="email"
+                         required
+                         name="email"
                          placeholder="johndoe@example.com"
                          class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                        />
@@ -125,7 +214,8 @@ const Contact = () => {
                          Message
                        </label>
                        <textarea
-                         {...register("message")}
+                         name="message"
+                         required
                          class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                          placeholder="Message"
                        ></textarea>
